4fb1295f265314710b27e544c068ea3dcdc1fc7f
Aryan Gupta <arynn.gupta@gmail.com>
2021-12-17 18:44:19 +0530

    first commit

---
+++ entry/src/main/java/com/cyb0rg/neumorphism_theme/MyApplication.java
@@ -0,0 +1,10 @@
+package com.cyb0rg.neumorphism_theme;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
